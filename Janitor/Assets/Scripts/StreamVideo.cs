014473501aa6d22be7e5b023dcee4f24325f61e6
Benjamin Roux <benjamin2.roux@epitech.eu>
2020-02-02 16:14:32 +0100

    add video into menu

---
+++ Janitor/Assets/Scripts/StreamVideo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class StreamVideo : MonoBehaviour
+{
+    public RawImage bg;
+    private VideoPlayer videoPlayer;
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+        StartCoroutine(PlayVideo());
+    }
+
+    IEnumerator PlayVideo() {
+        WaitForSeconds wf = new WaitForSeconds(1);
+        videoPlayer.Prepare();
+        while (!videoPlayer.isPrepared) {
+            yield return wf;
+            break;
+        }
+        bg.texture = videoPlayer.texture;
+        videoPlayer.Play();
+        audioSource.Play();
+    }
+}
