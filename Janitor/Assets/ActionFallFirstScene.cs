b68f6599a27b47bcd985e4dbccc4d2aa01109e50
unknown <robin.millian@epitech.eu>
2020-02-02 11:59:56 +0100

    Add: First level

---
+++ Janitor/Assets/ActionFallFirstScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionFallFirstScene : ActionScript
+{
+    private Rigidbody[] rigidbodies;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbodies = GetComponentsInChildren<Rigidbody>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void makeAction() {
+        for (int i = 0; i < rigidbodies.Length; i++) {
+            rigidbodies[i].isKinematic = false;
+        }
+    }
+}
